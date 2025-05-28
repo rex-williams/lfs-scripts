5438be476e70e62a51c61a2f8afd701227ebda29
Rex Williams <degenincel070@proton.me>
2025-05-27 21:28:12 -0500

    09:28PM

--- acl.sh
+++ acl.sh
@@ -5,3 +5,8 @@ TMPDIR="/tmp/build"
 [ -d "$TMPDIR" ] || mkdir "$TMPDIR"
 tar xvf "/sources/acl-${version}.tar.xz" -C "$TMPDIR"
 cd $TMPDIR/acl-${version} || exit 1
+/configure --prefix=/usr         \
+            --docdir=/usr/share/doc/acl-2.3.2
+make || exit 1
+make check || exit 1
+make install
