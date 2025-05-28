8f6495f46aa6073e33b7e9a955207d469c3a057a
Rex Williams <degenincel070@proton.me>
2025-05-27 21:17:10 -0500

    09:17PM

--- bzip2.sh
+++ bzip2.sh
@@ -6,4 +6,16 @@ TMPDIR="/tmp/build"
 tar xvf "/sources/bzip2-${version}.tar.gz" -C "$TMPDIR"
 cp -fv "/sources/bzip2-${version}-install_docs-1.patch" "$TMPDIR"
 cd $TMPDIR/bzip2-${version} || exit 1
-patch -Np1 -i ../bzip2-1.0.8-install_docs-1.patch
+patch -Np1 -i ../bzip2-${version}-install_docs-1.patch
+sed -i 's@\(ln -s -f \)$(PREFIX)/bin/@\1@' Makefile
+sed -i "s@(PREFIX)/man@(PREFIX)/share/man@g" Makefile
+make -f Makefile-libbz2_so
+make clean
+make || exit 1
+make PREFIX=/usr install || exit 1
+cp -av libbz2.so.* /usr/lib
+ln -sv libbz2.so.1.0.8 /usr/lib/libbz2.so
+cp -v bzip2-shared /usr/bin/bzip2
+for i in /usr/bin/{bzcat,bunzip2}; do
+  ln -sfv bzip2 $i
+done
