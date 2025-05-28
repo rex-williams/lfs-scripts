6dc8202116e97501383711380671804876af8814
Rex Williams <degenincel070@proton.me>
2025-05-27 21:21:24 -0500

    09:21PM

--- gzip.sh
+++ gzip.sh
@@ -3,7 +3,7 @@ set -x
 version="1.13"
 TMPDIR="/tmp/build"
 [ -d "$TMPDIR" ] || mkdir "$TMPDIR"
-tar xvf "/sources/gzip-${version}.tar.gz" -C "$TMPDIR"
+tar xvf "/sources/gzip-${version}.tar.xz" -C "$TMPDIR"
 cd $TMPDIR/gzip-${version} || exit 1
 ./configure --prefix=/usr
 make || exit 1
