3eff72752236de4f7c6c7ac15a8bb1c97a810df4
Rex Williams <degenincel070@proton.me>
2025-05-27 21:19:11 -0500

    09:19PM

--- zlib.sh
+++ zlib.sh
@@ -5,7 +5,7 @@ TMPDIR="/tmp/build"
 [ -d "$TMPDIR" ] || mkdir "$TMPDIR"
 tar xvf "/sources/zlib-${version}.tar.gz" -C "$TMPDIR"
 cd $TMPDIR/zlib-${version} || exit 1
-/configure --prefix=/usr
+./configure --prefix=/usr
 make || exit 1
 make check || exit 1
 make install || exit 1
