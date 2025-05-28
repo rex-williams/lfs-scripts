2c49004ff695b231a12ca173204ac26a06d5de29
Rex Williams <degenincel070@proton.me>
2025-05-27 21:27:30 -0500

    09:27PM

--- acl.sh
+++ acl.sh
@@ -3,5 +3,5 @@ set -x
 version="2.3.2"
 TMPDIR="/tmp/build"
 [ -d "$TMPDIR" ] || mkdir "$TMPDIR"
-tar xvf "/sources/acl-${version}.tar.gz" -C "$TMPDIR"
+tar xvf "/sources/acl-${version}.tar.xz" -C "$TMPDIR"
 cd $TMPDIR/acl-${version} || exit 1
