f98228cd0b6e320ac150498ceba582e5f7fec006
Rex Williams <degenincel070@proton.me>
2025-05-27 20:59:33 -0500

    08:59PM

--- push.sh
+++ push.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-git add .
-git commit -m "$(clock)"
-git push
-doas cp -f ./* /mnt/scripts
+{
+    git add .
+    git commit -m "$(clock)"
+    git push
+} &
+doas cp -fv ./* /mnt/scripts &
+wait
