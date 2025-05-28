0959316dedfc9b4956b351afca67a514fb619500
Rex Williams <degenincel070@proton.me>
2025-05-27 21:45:02 -0500

    09:45PM

--- locale.sh
+++ locale.sh
@@ -3,6 +3,6 @@ set -x
 version="2.41"
 TMPDIR="/tmp/build"
 [ -d "$TMPDIR" ] || mkdir "$TMPDIR"
-tar xvf "/sources/glibc-${version}.tar.gz" -C "$TMPDIR"
+tar xvf "/sources/glibc-${version}.tar.xz" -C "$TMPDIR"
 cd $TMPDIR/glibc-${version} || exit 1
 localedef -i C -f UTF-8 C.UTF-8
