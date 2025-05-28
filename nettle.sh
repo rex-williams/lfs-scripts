39ef2339222f5d4dbc170c56e4f68a4ee6027d6c
Rex Williams <degenincel070@proton.me>
2025-05-27 21:07:08 -0500

    09:07PM

--- nettle.sh
+++ nettle.sh
@@ -4,3 +4,10 @@ TMPDIR="/tmp/build"
 [ -d "$TMPDIR" ] || mkdir "$TMPDIR"
 tar xvf /sources/nettle-${version}.tar.gz -C "$TMPDIR"
 cd $TMPDIR/nettle-${version} || exit 1
+./configure \
+            --prefix=/usr &&
+make || exit 1
+make install &&
+chmod   -v   755 /usr/lib/lib{hogweed,nettle}.so &&
+install -v -m755 -d /usr/share/doc/nettle-3.10.1 &&
+install -v -m644 nettle.{html,pdf} /usr/share/doc/nettle-3.10.1
