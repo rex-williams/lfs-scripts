709a82ee3778600c8b77b83efa7e86e8232d416e
Rex Williams <degenincel070@proton.me>
2025-05-27 21:27:07 -0500

    09:27PM

---
+++ acl.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -x
+version="2.3.2"
+TMPDIR="/tmp/build"
+[ -d "$TMPDIR" ] || mkdir "$TMPDIR"
+tar xvf "/sources/acl-${version}.tar.gz" -C "$TMPDIR"
+cd $TMPDIR/acl-${version} || exit 1
