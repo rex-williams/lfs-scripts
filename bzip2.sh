3ca920f971697025d4ed322bc03e3ebe2adb5ead
Rex Williams <degenincel070@proton.me>
2025-05-27 21:16:02 -0500

    09:16PM

--- bzip2.sh
+++ bzip2.sh
@@ -4,6 +4,6 @@ version="1.0.8"
 TMPDIR="/tmp/build"
 [ -d "$TMPDIR" ] || mkdir "$TMPDIR"
 tar xvf "/sources/bzip2-${version}.tar.gz" -C "$TMPDIR"
-cp -fv "/sources/bzip2-${version}-install_docs-1.patch" "$TMPDIR/bzip2-${version}"
+cp -fv "/sources/bzip2-${version}-install_docs-1.patch" "$TMPDIR"
 cd $TMPDIR/bzip2-${version} || exit 1
 patch -Np1 -i ../bzip2-1.0.8-install_docs-1.patch
