f2a62b89e35a4a3ef6d5004977281338529fb34a
Rex Williams <degenincel070@proton.me>
2025-05-27 21:02:57 -0500

    09:02PM

---
+++ lzo.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+version="2.10"
+TMPDIR="/tmp/build"
+[ -d "$TMPDIR" ] || mkdir "$TMPDIR"
+tar xvf /sources/lzo-${version}.tar.gz -C "$TMPDIR"
+cd $TMPDIR/readline-${version} || exit 1
+./configure --prefix=/usr    \
+            --enable-shared  \
+            --docdir=/usr/share/doc/lzo-2.10 &&
+make || exit 1
+make install
