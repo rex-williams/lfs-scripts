726eee0f8cf99ed44855a04097a73fb507797155
Rex Williams <degenincel070@proton.me>
2025-05-27 21:18:30 -0500

    09:18PM

--- zlib.sh
+++ zlib.sh
@@ -4,5 +4,4 @@ version="1.3.1"
 TMPDIR="/tmp/build"
 [ -d "$TMPDIR" ] || mkdir "$TMPDIR"
 tar xvf "/sources/zlib-${version}.tar.gz" -C "$TMPDIR"
-cp -fv "/sources/zlib-${version}-install_docs-1.patch" "$TMPDIR"
 cd $TMPDIR/zlib-${version} || exit 1
