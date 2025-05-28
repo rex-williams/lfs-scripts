66c2c1e23db41cc9d8fa4243f20923026748d58c
Rex Williams <degenincel070@proton.me>
2025-05-27 21:12:21 -0500

    09:12PM

--- bzip2.sh
+++ bzip2.sh
@@ -3,5 +3,5 @@ set -x
 version="1.0.8"
 TMPDIR="/tmp/build"
 [ -d "$TMPDIR" ] || mkdir "$TMPDIR"
-tar xvf "/sources/bzip2-$(echo "$version" | sed 's/\./_/g').tar.gz" -C "$TMPDIR"
+tar xvf "/sources/bzip2-$version.tar.gz" -C "$TMPDIR"
 cd $TMPDIR/bzip2-${version} || exit 1
