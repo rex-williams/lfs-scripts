7509f0388135541f54721b030c9df7c280bf359d
Rex Williams <degenincel070@proton.me>
2025-05-27 21:35:36 -0500

    09:35PM

--- zstd.sh
+++ zstd.sh
@@ -3,7 +3,7 @@ set -x
 version="1.5.7"
 TMPDIR="/tmp/build"
 [ -d "$TMPDIR" ] || mkdir "$TMPDIR"
-tar xvf "/sources/zstd-${version}.tar.xz" -C "$TMPDIR"
+tar xvf "/sources/zstd-${version}.tar.gz" -C "$TMPDIR"
 cd $TMPDIR/zstd-${version} || exit 1
 make prefix=/usr || exit 1
 make check || exit 1
