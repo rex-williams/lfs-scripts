763fedaea546f5f4371ae8b5167cd3cc8d865095
Rex Williams <degenincel070@proton.me>
2025-06-01 06:07:36 -0500

    06:07AM

--- python3.sh
+++ python3.sh
@@ -3,7 +3,7 @@ set -x
 version="3.13.2"
 TMPDIR="/tmp/build"
 [ -d "$TMPDIR" ] || mkdir "$TMPDIR"
-tar xvf "/sources/Python-${version}.tar.gz" -C "$TMPDIR"
+tar xvf "/sources/Python-${version}.tar.xz" -C "$TMPDIR"
 cd $TMPDIR/Python-${version} || exit 1
 ./configure --prefix=/usr \
             --enable-shared \
