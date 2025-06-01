d947c58ef9e47f303b6eff9135c06a31ed8f72d4
Rex Williams <degenincel070@proton.me>
2025-06-01 06:09:05 -0500

    06:09AM

---
+++ pip3.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -x
+version="3.13.2"
+TMPDIR="/tmp/build"
+[ -d "$TMPDIR" ] || mkdir "$TMPDIR"
+tar xvf "/sources/Python-${version}.tar.xz" -C "$TMPDIR"
+cd $TMPDIR/Python-${version} || exit 1
+./configure --prefix=/usr \
+            --enable-shared
+make
+make install
