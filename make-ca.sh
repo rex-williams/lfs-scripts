ba60df2df0502cbbcfd41024cf2db79cfc5f0377
Rex Williams <degenincel070@proton.me>
2025-06-01 07:03:15 -0500

    07:03AM

--- make-ca.sh
+++ make-ca.sh
@@ -5,5 +5,11 @@ TMPDIR="/tmp/build"
 [ -d "$TMPDIR" ] || mkdir "$TMPDIR"
 tar xvf "/sources/make-ca-$version.tar.gz" -C "$TMPDIR"
 cd $TMPDIR/make-ca-$version
-make install &&
+make install
 install -vdm755 /etc/ssl/local
+/usr/sbin/make-ca -g
+cat > /etc/cron.weekly/update-pki.sh << "EOF" &&
+#!/bin/bash
+/usr/sbin/make-ca -g
+EOF
+chmod 754 /etc/cron.weekly/update-pki.sh
