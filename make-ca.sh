8639201945cdd3025677f27f6c1e0eb0a545af8a
Rex Williams <degenincel070@proton.me>
2025-06-01 06:29:10 -0500

    06:29AM

--- make-ca.sh
+++ make-ca.sh
@@ -3,7 +3,7 @@ set -x
 version="1.16"
 TMPDIR="/tmp/build"
 [ -d "$TMPDIR" ] || mkdir "$TMPDIR"
-tar xvf "/sources/make-ca-${version}.tar.xz" -C "$TMPDIR"
+tar xvf "/sources/make-ca-${version}.tar.gz" -C "$TMPDIR"
 cd $TMPDIR/make-ca-${version} || exit 1
 make install &&
 install -vdm755 /etc/ssl/local
