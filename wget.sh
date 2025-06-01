f5e71ee47b5e84b93f452c6181cc48dee8264b5c
Rex Williams <degenincel070@proton.me>
2025-06-01 06:29:34 -0500

    06:29AM

--- wget.sh
+++ wget.sh
@@ -3,7 +3,7 @@ set -x
 version="1.25.0"
 TMPDIR="/tmp/build"
 [ -d "$TMPDIR" ] || mkdir "$TMPDIR"
-tar xvf "/sources/wget-${version}.tar.xz" -C "$TMPDIR"
+tar xvf "/sources/wget-${version}.tar.gz" -C "$TMPDIR"
 cd $TMPDIR/wget-${version} || exit 1
 ./configure --prefix=/usr      \
             --sysconfdir=/etc  \
